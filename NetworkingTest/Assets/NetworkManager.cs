b012ce543f1f4b451c129bf548efffb633f2013b
sunafrus <hugo.matthews@googlemail.com>
2015-02-23 19:29:16 +0000

    Base Network Functionality and Movement
    Server can be started and joined as long as port 25565 has been
    forwarded on host machine.
    
    Interpolation added to state synchronisation due to testing without
    revealing position of other players skipping due to latency issues.
    
    Includes implementation of base 'tank' style movement for player.

---
+++ NetworkingTest/Assets/NetworkManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+	private const string typeName = "VeryUniqueTopDownGame";
+	private const string gameName = "join Lobby";
+
+	private HostData[] hostList;
+
+	public GameObject playerPrefab;
+	
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25565, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}	
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+		SpawnPlayer();
+	}
+
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Connected to Server");
+		SpawnPlayer();
+	}
+
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(playerPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+	}
+	
+}
---
+++ NetworkingTest/Assets/PlayerMobility.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMobility : MonoBehaviour
+{
+	public float speed;
+
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+	private Quaternion syncStartRotation;
+	private Quaternion syncEndRotation;
+
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+		if (networkView.isMine)
+		{
+			InputMovement ();
+		}
+		else
+		{
+			SyncedMovement ();
+		}
+	}
+
+	private void InputMovement ()
+	{
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+		
+		transform.rotation = rot;
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		rigidbody2D.angularVelocity = 0;
+		
+		//Movement toward/away from the mouse
+		float verticalInput = Input.GetAxis ("Vertical");
+		rigidbody2D.AddForce (gameObject.transform.up * speed * verticalInput);
+		
+		//Strafing around the mouse
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		rigidbody2D.AddForce (gameObject.transform.right * speed/2 *  horizontalInput);
+	}
+
+	private void SyncedMovement ()
+	{
+		syncTime += Time.deltaTime;
+
+		rigidbody2D.position = Vector3.Lerp (syncStartPosition, syncEndPosition, syncTime / syncDelay);
+
+		transform.rotation = Quaternion.Lerp (syncStartRotation, syncEndRotation, syncTime / syncDelay);
+	}
+
+	void OnSerializeNetworkView (BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Quaternion syncRotation = Quaternion.identity;
+
+		if (stream.isWriting)
+		{
+			syncPosition = rigidbody2D.position;
+			stream.Serialize (ref syncPosition);
+
+			syncRotation = transform.rotation;
+			stream.Serialize (ref syncRotation);
+		}
+		else
+		{
+			stream.Serialize (ref syncPosition);
+
+			stream.Serialize (ref syncRotation);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncStartPosition = rigidbody2D.position;
+			syncEndPosition = syncPosition;
+
+			syncStartRotation = transform.rotation;
+			syncEndRotation = syncRotation;
+		}
+	}
+
+}
